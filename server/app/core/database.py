0d2d783c32de04524c690d137446e35e0c35a0a7
adhi0987 <adhithyashivakumar2511@gmail.com>
2026-03-04 22:39:34 +0530

    added gitignore

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -2,11 +2,12 @@ import os
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
 import uuid
 import datetime
 
-# Replace with your actual Supabase Postgres URL
-DATABASE_URL = "postgresql://postgres.clgjswrlcwzmdxhhegtk:[YOUR-PASSWORD]@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -24,15 +25,12 @@ class MovementData(Base):
     __tablename__ = "movement_data"
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey("personal_information.id"))
-    
-    # Updated 6-axis data
     a_x = Column(Float)
     a_y = Column(Float)
     a_z = Column(Float)
     g_x = Column(Float)
     g_y = Column(Float)
     g_z = Column(Float)
-    
     label = Column(String, default="untagged")
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
 
